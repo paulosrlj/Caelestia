53ac0cc039ce4ce0d12205683cb6b8b8dc833451
Paulo <paulo.leite@academico.ifpb.edu.br>
2022-06-12 17:11:00 -0300

    feat: Module entity

---
+++ src/main/java/com/ifpb/caelestiabackend/domain/entities/Module.java
@@ -0,0 +1,40 @@
+package com.ifpb.caelestiabackend.domain.entities;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Module {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Integer qtyLessons;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Module module = (Module) o;
+        return id != null && Objects.equals(id, module.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
