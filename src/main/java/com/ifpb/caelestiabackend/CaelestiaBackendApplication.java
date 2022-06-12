4b61270ecbcb91b19b16da1cc39d05b980ffb8df
Paulo <paulo@pop-os.localdomain>
2022-06-12 16:59:11 -0300

    chore: initial commit

---
+++ src/main/java/com/ifpb/caelestiabackend/CaelestiaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ifpb.caelestiabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaelestiaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaelestiaBackendApplication.class, args);
+	}
+
+}
