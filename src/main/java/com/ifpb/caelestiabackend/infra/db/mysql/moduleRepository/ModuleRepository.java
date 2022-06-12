66fb9d2c4e6461d163aa4400400352fbacd4c825
Paulo <paulo.leite@academico.ifpb.edu.br>
2022-06-12 17:15:28 -0300

    feat: Module repository

---
+++ src/main/java/com/ifpb/caelestiabackend/infra/db/mysql/moduleRepository/ModuleRepository.java
@@ -0,0 +1,7 @@
+package com.ifpb.caelestiabackend.infra.db.mysql.moduleRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ModuleRepository extends JpaRepository<Module, Long> {
+
+}
