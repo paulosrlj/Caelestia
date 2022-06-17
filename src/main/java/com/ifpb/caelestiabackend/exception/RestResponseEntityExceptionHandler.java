acb8a57a3a088793fbed02956583cc503a1cf41e
Paulo <paulo.leite@academico.ifpb.edu.br>
2022-06-17 13:33:15 -0300

    feat: add handleMethodArgumentNotValid to catch validaitons exceptions

--- src/main/java/com/ifpb/caelestiabackend/exception/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/ifpb/caelestiabackend/exception/RestResponseEntityExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.ifpb.caelestiabackend.exception;
 
 import com.ifpb.caelestiabackend.domain.entities.HttpErrorMessage;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,9 +16,22 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(MissingParamException.class)
-    public ResponseEntity<HttpErrorMessage> departmentNotFoundException(MissingParamException exception, WebRequest request) {
-        HttpErrorMessage message = new HttpErrorMessage(HttpStatus.NOT_FOUND, exception.getMessage());
+    public ResponseEntity<HttpErrorMessage> departmentNotFoundException(MissingParamException ex, WebRequest request) {
+        HttpErrorMessage message = new HttpErrorMessage(HttpStatus.NOT_FOUND, ex.getMessage());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
     }
+
+    @Override
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+        HttpErrorMessage message = new HttpErrorMessage(HttpStatus.BAD_REQUEST, ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
+    }
+
 }
