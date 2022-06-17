c8239d3f25ee8b7f4491e74c4280e31a15aacbed
Paulo <paulo.leite@academico.ifpb.edu.br>
2022-06-16 22:40:35 -0300

    feat: ModuleController interface

---
+++ src/main/java/com/ifpb/caelestiabackend/controller/module/IModuleController.java
@@ -0,0 +1,8 @@
+package com.ifpb.caelestiabackend.controller.module;
+
+import com.ifpb.caelestiabackend.controller.Controller;
+import com.ifpb.caelestiabackend.domain.entities.Module;
+
+public interface IModuleController extends Controller<Module> {
+
+}
