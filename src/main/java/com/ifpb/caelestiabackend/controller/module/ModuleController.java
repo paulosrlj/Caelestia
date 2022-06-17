80a439d32e50a55a4963dd2e2e462f2e6eea8f37
Paulo <paulo.leite@academico.ifpb.edu.br>
2022-06-16 22:40:12 -0300

    feat: ModuleController

---
+++ src/main/java/com/ifpb/caelestiabackend/controller/module/ModuleController.java
@@ -0,0 +1,27 @@
+package com.ifpb.caelestiabackend.controller.module;
+
+import com.ifpb.caelestiabackend.domain.entities.Module;
+import com.ifpb.caelestiabackend.services.module.ModuleService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/module")
+public class ModuleController implements IModuleController {
+
+    private final ModuleService moduleService;
+
+    public ModuleController(ModuleService moduleService) {
+        this.moduleService = moduleService;
+    }
+
+    @Override
+    @PostMapping(value = "/")
+    public ResponseEntity<Module> add(Module module) {
+        Module modulePersisted = moduleService.add(module);
+        return ResponseEntity.ok(modulePersisted);
+    }
+
+}
