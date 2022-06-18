e2669a305337107d528e7ba079972896db973cab
Paulo <paulo.leite@academico.ifpb.edu.br>
2022-06-17 23:11:22 -0300

    feat: change interfaces signature to Dto

--- src/main/java/com/ifpb/caelestiabackend/controller/module/IModuleController.java
+++ src/main/java/com/ifpb/caelestiabackend/controller/module/IModuleController.java
@@ -1,8 +1,11 @@
 package com.ifpb.caelestiabackend.controller.module;
 
 import com.ifpb.caelestiabackend.domain.entities.Module;
+import com.ifpb.caelestiabackend.dto.ModuleDto;
 import org.springframework.http.ResponseEntity;
 
+import javax.validation.Valid;
+
 public interface IModuleController {
-    ResponseEntity<Module> add(Module module);
+    ResponseEntity<Module> add(@Valid ModuleDto moduleDto);
 }
--- src/main/java/com/ifpb/caelestiabackend/domain/usecases/module/AddModule.java
+++ src/main/java/com/ifpb/caelestiabackend/domain/usecases/module/AddModule.java
@@ -1,7 +1,8 @@
 package com.ifpb.caelestiabackend.domain.usecases.module;
 
 import com.ifpb.caelestiabackend.domain.entities.Module;
+import com.ifpb.caelestiabackend.dto.ModuleDto;
 
 public interface AddModule {
-    Module add(Module module);
+    Module add(ModuleDto moduleDto);
 }
