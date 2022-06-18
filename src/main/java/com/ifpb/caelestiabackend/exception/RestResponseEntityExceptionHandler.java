d194d012bcfca2e8707bd3be9b087c7073b611d0
Paulo <paulo.leite@academico.ifpb.edu.br>
2022-06-17 23:13:36 -0300

    feat: add errors to http response

--- src/main/java/com/ifpb/caelestiabackend/exception/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/ifpb/caelestiabackend/exception/RestResponseEntityExceptionHandler.java
@@ -1,26 +1,31 @@
 package com.ifpb.caelestiabackend.exception;
 
 import com.ifpb.caelestiabackend.domain.entities.HttpErrorMessage;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
 @ResponseStatus
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(MissingParamException.class)
-    public ResponseEntity<HttpErrorMessage> departmentNotFoundException(MissingParamException ex, WebRequest request) {
-        HttpErrorMessage message = new HttpErrorMessage(HttpStatus.NOT_FOUND, ex.getMessage());
-
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
-    }
+//    @ExceptionHandler(MissingParamException.class)
+//    @ResponseStatus(HttpStatus.BAD_REQUEST)
+//    public ResponseEntity<HttpErrorMessage> handleMissingParamException(MissingParamException ex, WebRequest request) {
+//        HttpErrorMessage message = new HttpErrorMessage(HttpStatus.NOT_FOUND, ex.getMessage());
+//
+//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
+//    }
 
     @Override
     @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -29,8 +34,13 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
             HttpHeaders headers,
             HttpStatus status,
             WebRequest request) {
-        HttpErrorMessage message = new HttpErrorMessage(HttpStatus.BAD_REQUEST, ex.getMessage());
 
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage).toList();
+
+        HttpErrorMessage message = new HttpErrorMessage(HttpStatus.BAD_REQUEST, "Erro de validação", errors);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
     }
 
