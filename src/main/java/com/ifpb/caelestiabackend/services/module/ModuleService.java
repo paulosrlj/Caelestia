570e5b1ae3d392ef4077e81dda1f2b54d6746765
Paulo <paulo.leite@academico.ifpb.edu.br>
2022-06-18 18:43:56 -0300

    feat: copy properties from dto to jpa entity

--- src/main/java/com/ifpb/caelestiabackend/services/module/ModuleService.java
+++ src/main/java/com/ifpb/caelestiabackend/services/module/ModuleService.java
@@ -2,7 +2,9 @@ package com.ifpb.caelestiabackend.services.module;
 
 import com.ifpb.caelestiabackend.domain.entities.Module;
 import com.ifpb.caelestiabackend.domain.usecases.module.AddModule;
+import com.ifpb.caelestiabackend.dto.ModuleDto;
 import com.ifpb.caelestiabackend.repository.ModuleRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,7 +17,11 @@ public class ModuleService implements AddModule {
     }
 
     @Override
-    public Module add(Module module) {
+    public Module add(ModuleDto moduleDto) {
+        Module module = new Module();
+
+        BeanUtils.copyProperties(moduleDto, module);
+
         return moduleRepository.save(module);
     }
 }
