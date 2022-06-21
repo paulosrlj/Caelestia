a7a2cb8cea95bcd0fefbfb6dd0070684e675638b
Paulo <paulo.leite@academico.ifpb.edu.br>
2022-06-21 11:26:40 -0300

    refactor: add correct return types

--- src/main/java/com/ifpb/caelestiabackend/controller/module/IModuleController.java
+++ src/main/java/com/ifpb/caelestiabackend/controller/module/IModuleController.java
@@ -1,14 +1,13 @@
 package com.ifpb.caelestiabackend.controller.module;
 
 import com.ifpb.caelestiabackend.domain.entities.Module;
-import com.ifpb.caelestiabackend.dto.ModuleDto;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.validation.Valid;
+import java.util.Map;
 
 public interface IModuleController {
-    ResponseEntity<Module> add(@Valid ModuleDto moduleDto);
+    ResponseEntity<Map<String, Object>> add(@Valid Module module);
 
     ResponseEntity<?> delete(Long id);
 
--- src/main/java/com/ifpb/caelestiabackend/domain/usecases/module/AddModule.java
+++ src/main/java/com/ifpb/caelestiabackend/domain/usecases/module/AddModule.java
@@ -1,8 +1,7 @@
 package com.ifpb.caelestiabackend.domain.usecases.module;
 
 import com.ifpb.caelestiabackend.domain.entities.Module;
-import com.ifpb.caelestiabackend.dto.ModuleDto;
 
 public interface AddModule {
-    Module add(ModuleDto moduleDto);
+    Module add(Module module);
 }
