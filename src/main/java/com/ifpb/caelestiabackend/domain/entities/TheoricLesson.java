14f5614cf91a8c13296e2de16f6c4e12b27a2e0d
Paulo <paulo.leite@academico.ifpb.edu.br>
2022-06-21 15:30:05 -0300

    feat: add validations

--- src/main/java/com/ifpb/caelestiabackend/domain/entities/TheoricLesson.java
+++ src/main/java/com/ifpb/caelestiabackend/domain/entities/TheoricLesson.java
@@ -1,8 +1,10 @@
 package com.ifpb.caelestiabackend.domain.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
@@ -18,13 +20,19 @@ public class TheoricLesson {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Min(value = 0, message = "O valor de Xp não pode ser menor que 0")
+    @NotNull(message = "O valor de Xp não pode ser nulo!")
     private Long xpEarned;
 
+    @NotNull(message = "O nome da lição não pode ser nulo!")
+    @NotBlank(message = "O nome da lição não pode estar em branco!")
     private String lessonName;
 
+    @NotNull(message = "A descrição não pode estar em branco!")
     private String description;
 
     @ManyToOne
     @JoinColumn(name = "module_id")
+    @JsonIgnore
     private Module module;
 }
