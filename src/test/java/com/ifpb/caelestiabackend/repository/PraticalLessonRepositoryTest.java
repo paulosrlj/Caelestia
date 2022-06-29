12c40094e80e9815a97e06e78d8bf47f56f38bf4
paulo <paulo.leite@academico.ifpb.edu.br>
2022-06-29 14:58:53 -0300

    test: mustSaveAPraticalLesson

---
+++ src/test/java/com/ifpb/caelestiabackend/repository/PraticalLessonRepositoryTest.java
@@ -0,0 +1,51 @@
+package com.ifpb.caelestiabackend.repository;
+
+import com.ifpb.caelestiabackend.domain.entities.Module;
+import com.ifpb.caelestiabackend.domain.entities.PraticalLesson.PraticalLesson;
+import com.ifpb.caelestiabackend.domain.entities.TheoricLesson;
+import com.ifpb.caelestiabackend.util.ModuleFactory;
+import com.ifpb.caelestiabackend.util.PraticalLessonFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+
+@DataJpaTest
+@DisplayName("Pratical Lesson repository tests")
+class PraticalLessonRepositoryTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PraticalLessonRepositoryTest.class);
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Autowired
+    private PraticalLessonRepository praticalLessonRepository;
+
+    @Autowired
+    private ModuleRepository moduleRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    public void mustSaveAPraticalLesson() {
+        PraticalLesson pl = PraticalLessonFactory.makePraticalLesson();
+
+        Module module = ModuleFactory.makeModule();
+        Module persistedModule = moduleRepository.save(module);
+
+        pl.setModule(persistedModule);
+        PraticalLesson persistedPl = praticalLessonRepository.save(pl);
+
+        Assertions.assertThat(persistedPl.getId()).isNotNull();
+        Assertions.assertThat(persistedPl.getModule().getId()).isEqualTo(persistedModule.getId());
+    }
+}
