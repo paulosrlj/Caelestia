600d321d8c56f37acbae367cbf6770f72ced221e
paulo <paulo.leite@academico.ifpb.edu.br>
2022-06-29 22:35:56 -0300

    feat: shouldFindPraticalLessonById

--- src/main/java/com/ifpb/caelestiabackend/services/praticalLesson/PraticalLessonService.java
+++ src/main/java/com/ifpb/caelestiabackend/services/praticalLesson/PraticalLessonService.java
@@ -5,9 +5,9 @@ import com.ifpb.caelestiabackend.domain.entities.PraticalLesson.PraticalLesson;
 import com.ifpb.caelestiabackend.domain.entities.TheoricLesson;
 import com.ifpb.caelestiabackend.domain.usecases.module.AddModule;
 import com.ifpb.caelestiabackend.domain.usecases.module.DeleteModule;
-import com.ifpb.caelestiabackend.domain.usecases.module.GetById;
 import com.ifpb.caelestiabackend.domain.usecases.module.UpdateModule;
 import com.ifpb.caelestiabackend.domain.usecases.praticalLesson.AddPraticalLesson;
+import com.ifpb.caelestiabackend.domain.usecases.praticalLesson.GetById;
 import com.ifpb.caelestiabackend.repository.ModuleRepository;
 import com.ifpb.caelestiabackend.repository.PraticalLessonRepository;
 import org.springframework.beans.BeanUtils;
@@ -19,7 +19,7 @@ import java.util.Optional;
 import java.util.Set;
 
 @Service
-public class PraticalLessonService implements AddPraticalLesson {
+public class PraticalLessonService implements AddPraticalLesson, GetById {
 
     private final PraticalLessonRepository praticalLessonRepository;
 
@@ -31,5 +31,16 @@ public class PraticalLessonService implements AddPraticalLesson {
     @Override
     public PraticalLesson add(PraticalLesson praticalLesson) {
         return praticalLessonRepository.save(praticalLesson);
+    }
+
+    @Override
+    public PraticalLesson getById(Long id) {
+        Optional<PraticalLesson> praticalLesson = praticalLessonRepository.findById(id);
+
+        if (praticalLesson.isEmpty()) {
+            throw new EntityNotFoundException(String.format("A lição prática de Id %d não existe.", id));
+        }
+
+        return praticalLesson.get();
     }
 }
