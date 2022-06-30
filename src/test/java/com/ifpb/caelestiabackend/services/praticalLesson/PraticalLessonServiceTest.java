0b09a8c98cb76a53ccee80be3ea4602554d02d81
paulo <paulo.leite@academico.ifpb.edu.br>
2022-06-29 22:23:31 -0300

    test: shouldAddAPraticalLesson

---
+++ src/test/java/com/ifpb/caelestiabackend/services/praticalLesson/PraticalLessonServiceTest.java
@@ -0,0 +1,55 @@
+package com.ifpb.caelestiabackend.services.praticalLesson;
+
+import com.ifpb.caelestiabackend.domain.entities.Module;
+import com.ifpb.caelestiabackend.domain.entities.PraticalLesson.PraticalLesson;
+import com.ifpb.caelestiabackend.repository.ModuleRepository;
+import com.ifpb.caelestiabackend.repository.PraticalLessonRepository;
+import com.ifpb.caelestiabackend.util.ModuleFactory;
+import com.ifpb.caelestiabackend.util.PraticalLessonFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("Pratical Lesson tests")
+class PraticalLessonServiceTest {
+
+    @Autowired
+    private PraticalLessonService praticalLessonService;
+
+    @MockBean
+    private PraticalLessonRepository praticalLessonRepository;
+
+    @MockBean
+    private ModuleRepository moduleRepository;
+
+    @Test
+    public void shouldAddAPraticalLesson() {
+        Module module = ModuleFactory.makeModule();
+        PraticalLesson pl = PraticalLessonFactory.makePraticalLesson();
+
+        Mockito.when(moduleRepository.save(ArgumentMatchers.any(Module.class)))
+                .thenReturn(ModuleFactory.makePersistedModule());
+        Mockito.when(praticalLessonRepository.save(ArgumentMatchers.any(PraticalLesson.class)))
+                .thenReturn(PraticalLessonFactory.makePersistedPraticalLesson());
+
+        Module persistedModule = moduleRepository.save(module);
+        pl.setModule(persistedModule);
+
+        PraticalLesson plPersisted = praticalLessonService.add(pl);
+
+        Assertions.assertThat(plPersisted.getId()).isNotNull();
+        Assertions.assertThat(plPersisted.getCreatedAt()).isNotNull();
+        Assertions.assertThat(plPersisted.getUpdatedAt()).isNotNull();
+    }
+}
