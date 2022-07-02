c3539b417d5af7da47786ebae263029eb2c3d991
paulo <paulo.leite@academico.ifpb.edu.br>
2022-07-01 21:36:18 -0300

    feat: add CopyNotNullProperties util

---
+++ src/main/java/com/ifpb/caelestiabackend/util/CopyNotNullProperties.java
@@ -0,0 +1,36 @@
+package com.ifpb.caelestiabackend.util;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CopyNotNullProperties {
+
+    public static void copyNonNullProperties(Object source, Object destination){
+        BeanUtils.copyProperties(source, destination,
+                getNullPropertyNames(source));
+    }
+
+    /**
+     * Returns an array of null properties of an object
+     * @param source
+     * @return String[]
+     */
+    private static String[] getNullPropertyNames(Object source) {
+        BeanWrapper src = new BeanWrapperImpl(source);
+        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
+        Set<String> emptyNames = new HashSet<>();
+
+        for(java.beans.PropertyDescriptor pd : pds) {
+            //check if value of this property is null then add it to the collection
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue == null) emptyNames.add(pd.getName());
+        }
+        String[] result = new String[emptyNames.size()];
+        return emptyNames.toArray(result);
+    }
+
+}
