b418e35a94a49813e60b5653bd667c1383b5d0f8
paulo <paulo.leite@academico.ifpb.edu.br>
2022-07-04 22:20:42 -0300

    test: shouldReturn200IfValidDataIsProvided & shouldThrowExceptionIfNoValidDataIsProvided

--- src/test/java/com/ifpb/caelestiabackend/controller/praticalLesson/PraticalLessonControllerTest.java
+++ src/test/java/com/ifpb/caelestiabackend/controller/praticalLesson/PraticalLessonControllerTest.java
@@ -22,9 +22,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import javax.persistence.EntityNotFoundException;
-import java.util.Optional;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -44,28 +44,33 @@ class PraticalLessonControllerTest {
 
     @Test
     public void shouldReturn200IfValidDataIsProvided() throws Exception {
+        PraticalLesson validData = PraticalLessonFactory.makePraticalLesson();
+        Module module = new Module();
+        module.setId(1L);
+        validData.setModule(module);
+
         PraticalLesson expectedPl = PraticalLessonFactory.makePersistedPraticalLesson();
+        expectedPl.setModule(module);
 
-        Mockito.when(praticalLessonService.add(ArgumentMatchers.any(PraticalLesson.class)))
-                .thenReturn(expectedPl);
-
-        Mockito.when(praticalLessonRepository.save(ArgumentMatchers.any(PraticalLesson.class)))
+        Mockito.when(praticalLessonService.add(ArgumentMatchers.eq(validData)))
                 .thenReturn(expectedPl);
 
         RequestBuilder request = MockMvcRequestBuilders
                 .post("/pratical-lesson/")
                 .accept(MediaType.APPLICATION_JSON)
-                .content("{" +
-                        "\"lessonName\": \"Nome qualquer\",\n" +
-                        "\t\"description\": \"Descafisaifas\",\n" +
-                        "\t\"xpEarned\": 22,\n" +
-                        "\t\"answers\": {\n" +
-                        "\t\t\"answer1\": \"Resposta1\",\n" +
-                        "\t\t\"answer2\": \"Resposta2\",\n" +
-                        "\t\t\"answer3\": \"Resposta3\",\n" +
-                        "\t\t\"answer4\": \"Resposta4\",\n" +
-                        "\t\t\"correctAnswer\": 2\n" +
-                        "\t}}")
+                .content("""
+                        {"module": {
+                        \t\t"id": 1
+                        \t},"lessonName": "Lição prática 1",
+                        \t"description": "Quantos planetas existem no sistema solar?",
+                        \t"xpEarned": 150,
+                        \t"answers": {
+                        \t\t"answer1": "Resposta 1",
+                        \t\t"answer2": "Resposta 2",
+                        \t\t"answer3": "Resposta 3",
+                        \t\t"answer4": "Resposta 4",
+                        \t\t"correctAnswer": 2
+                        \t}}""")
                 .contentType(MediaType.APPLICATION_JSON);
 
         mockMvc.perform(request)
@@ -79,8 +84,6 @@ class PraticalLessonControllerTest {
         expectedPl.setModule(module);
 
         Mockito.when(praticalLessonService.getById(ArgumentMatchers.anyLong())).thenReturn(expectedPl);
-        Mockito.when(praticalLessonRepository.findById(ArgumentMatchers.anyLong()))
-                .thenReturn(Optional.of(expectedPl));
 
         RequestBuilder request = MockMvcRequestBuilders
                 .get("/pratical-lesson/{id}", 1L)
@@ -168,17 +171,41 @@ class PraticalLessonControllerTest {
                 .andReturn();
 
         Assertions.assertEquals("{\"id\":1," +
-                "\"lessonName\":\"Nome atualizado\"," +
-                "\"xpEarned\":150," +
-                "\"answers\":{" +
-                "\"answer1\":\"Resposta 1 Atualizada\"," +
-                "\"answer2\":\"Resposta 2\"," +
-                "\"answer3\":\"Resposta 3\"," +
-                "\"answer4\":\"Resposta 4\"," +
-                "\"correctAnswer\":2," +
-                "\"answersImages\":null" +
-                "}}"
+                        "\"lessonName\":\"Nome atualizado\"," +
+                        "\"xpEarned\":150," +
+                        "\"answers\":{" +
+                        "\"answer1\":\"Resposta 1 Atualizada\"," +
+                        "\"answer2\":\"Resposta 2\"," +
+                        "\"answer3\":\"Resposta 3\"," +
+                        "\"answer4\":\"Resposta 4\"," +
+                        "\"correctAnswer\":2," +
+                        "\"answersImages\":null" +
+                        "}}"
                 , result.getResponse().getContentAsString()
         );
+    }
+
+    @Test
+    public void shouldThrowExceptionIfNoValidDataIsProvided() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/pratical-lesson/")
+                .accept(MediaType.APPLICATION_JSON)
+                .content("""
+                        {"lessonName": "Lição prática 1",
+                        \t"description": "Quantos planetas existem no sistema solar?",
+                        \t"xpEarned": 150,
+                        \t"answers": {
+                        \t\t"answer1": "Resposta 1",
+                        \t\t"answer2": "Resposta 2",
+                        \t\t"answer3": "Resposta 3",
+                        \t\t"answer4": "Resposta 4",
+                        \t\t"correctAnswer": 2
+                        \t}}""")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(request)
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> Assertions.assertTrue(result.getResolvedException()
+                        instanceof MethodArgumentNotValidException));
     }
 }
