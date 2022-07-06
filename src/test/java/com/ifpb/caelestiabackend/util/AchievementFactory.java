c1bc5e393077dae349256c5827a3be21a53acc53
paulo <paulo.leite@academico.ifpb.edu.br>
2022-07-05 22:54:14 -0300

    refactor: factories methods

--- src/test/java/com/ifpb/caelestiabackend/util/AchievementFactory.java
+++ src/test/java/com/ifpb/caelestiabackend/util/AchievementFactory.java
@@ -2,8 +2,6 @@ package com.ifpb.caelestiabackend.util;
 
 import com.ifpb.caelestiabackend.domain.entities.Achievement.Achievement;
 import com.ifpb.caelestiabackend.domain.entities.Achievement.BonusType;
-import com.ifpb.caelestiabackend.domain.entities.Module;
-import com.ifpb.caelestiabackend.domain.entities.PraticalLesson.PraticalLesson;
 
 import java.math.BigDecimal;
 
@@ -15,16 +13,13 @@ public class AchievementFactory {
                 .description("+5% de xp em lições do tipo teórica")
                 .baseBonusPercentage(new BigDecimal("0.5"))
                 .bonusType(BonusType.THEORIC_LESSON)
+                .urlImage("https://urlqualquer.com.br")
                 .build();
     }
 
     public static Achievement makePersistedAchievement() {
-        return Achievement.builder()
-                .id(1L)
-                .achievementName("Observador Celeste")
-                .description("+5% de xp em lições do tipo teórica")
-                .baseBonusPercentage(new BigDecimal("0.5"))
-                .bonusType(BonusType.THEORIC_LESSON)
-                .build();
+        Achievement ac = makeAchievement();
+        ac.setId(1L);
+        return ac;
     }
 }
