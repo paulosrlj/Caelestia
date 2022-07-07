95e75e408ae3633fb05d3d2b8ae8d991b61c241a
paulo <paulo.leite@academico.ifpb.edu.br>
2022-07-06 21:59:19 -0300

    fix: add module validation on add an Achievement

--- src/main/java/com/ifpb/caelestiabackend/services/achievement/AchievementService.java
+++ src/main/java/com/ifpb/caelestiabackend/services/achievement/AchievementService.java
@@ -1,11 +1,13 @@
 package com.ifpb.caelestiabackend.services.achievement;
 
 import com.ifpb.caelestiabackend.domain.entities.Achievement.Achievement;
+import com.ifpb.caelestiabackend.domain.entities.Module;
 import com.ifpb.caelestiabackend.domain.usecases.achievement.AddAchievement;
 import com.ifpb.caelestiabackend.domain.usecases.achievement.DeleteAchievement;
 import com.ifpb.caelestiabackend.domain.usecases.achievement.GetAchievementById;
 import com.ifpb.caelestiabackend.domain.usecases.achievement.UpdateAchievement;
 import com.ifpb.caelestiabackend.repository.AchievementRepository;
+import com.ifpb.caelestiabackend.repository.ModuleRepository;
 import com.ifpb.caelestiabackend.util.CopyNotNullProperties;
 import org.springframework.stereotype.Service;
 
@@ -17,12 +19,22 @@ public class AchievementService implements AddAchievement, GetAchievementById, U
 
     private final AchievementRepository achievementRepository;
 
-    public AchievementService(AchievementRepository achievementRepository) {
+    private final ModuleRepository moduleRepository;
+
+    public AchievementService(AchievementRepository achievementRepository, ModuleRepository moduleRepository) {
         this.achievementRepository = achievementRepository;
+        this.moduleRepository = moduleRepository;
     }
 
     @Override
     public Achievement add(Achievement achievement) {
+        Optional<Module> moduleofLesson = moduleRepository.findById(achievement.getModule().getId());
+
+        if (moduleofLesson.isEmpty()) {
+            throw new EntityNotFoundException(String.format("O módulo de Id %d não existe.",
+                    achievement.getModule().getId()));
+        }
+
         return achievementRepository.save(achievement);
     }
 
