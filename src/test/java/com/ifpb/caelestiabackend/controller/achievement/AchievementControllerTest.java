6b634e7dd42f8540d633df54753c59fb2b33f9d0
paulo <paulo.leite@academico.ifpb.edu.br>
2022-07-06 22:44:12 -0300

    test: shouldReturnCorrectData

--- src/test/java/com/ifpb/caelestiabackend/controller/achievement/AchievementControllerTest.java
+++ src/test/java/com/ifpb/caelestiabackend/controller/achievement/AchievementControllerTest.java
@@ -6,6 +6,7 @@ import com.ifpb.caelestiabackend.repository.AchievementRepository;
 import com.ifpb.caelestiabackend.services.achievement.AchievementService;
 import com.ifpb.caelestiabackend.util.AchievementFactory;
 import com.ifpb.caelestiabackend.util.ModuleFactory;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,6 +20,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -70,5 +72,52 @@ class AchievementControllerTest {
 
         mockMvc.perform(request)
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldReturnCorrectData() throws Exception {
+        Module module = new Module();
+        module.setId(1L);
+        Achievement acToSave = AchievementFactory.makeAchievement();
+        Module moduleToReturn = new Module();
+        moduleToReturn.setId(1L);
+        moduleToReturn.setName("Modulo qualquer");
+        Achievement acPersisted = AchievementFactory.makePersistedAchievement();
+        acToSave.setModule(module);
+        acPersisted.setModule(moduleToReturn);
+
+        System.out.println(acToSave);
+        Mockito.when(achievementService.add(ArgumentMatchers.eq(acToSave))).thenReturn(acPersisted);
+        Mockito.when(achievementRepository.save(ArgumentMatchers.any(Achievement.class)))
+                .thenReturn(acPersisted);
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/achievement/")
+                .accept(MediaType.APPLICATION_JSON)
+                .content("{" +
+                        "\"achievementName\":\"Observador Celeste\"," +
+                        "\"description\": \"+5% de xp em lições do tipo teórica\"," +
+                        "\"baseBonusPercentage\": 0.5," +
+                        "\"bonusType\": \"THEORIC_LESSON\"," +
+                        "\"urlImage\": \"https://urlqualquer.com.br\"," +
+                        "\"module\": {\"id\":1}" +
+                        "}")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Assertions.assertEquals(
+                "{" +
+                        "\"id\":1," +
+                        "\"achievementName\":\"Observador Celeste\"," +
+                        "\"description\":\"+5% de xp em lições do tipo teórica\"," +
+                        "\"baseBonusPercentage\":0.5," +
+                        "\"bonusType\":\"THEORIC_LESSON\"," +
+                        "\"urlImage\":\"https://urlqualquer.com.br\"," +
+                        "\"module\":{\"id\":1,\"name\":\"Modulo qualquer\"}" +
+                        "}", result.getResponse().getContentAsString()
+        );
     }
 }
