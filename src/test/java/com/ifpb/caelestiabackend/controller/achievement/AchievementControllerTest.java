b3325e4999f99f06b0edfa3e951640d5cdc7d2d7
paulo <paulo.leite@academico.ifpb.edu.br>
2022-07-08 22:34:42 -0300

    test: shouldUpdateAndReturnAAchievement

--- src/test/java/com/ifpb/caelestiabackend/controller/achievement/AchievementControllerTest.java
+++ src/test/java/com/ifpb/caelestiabackend/controller/achievement/AchievementControllerTest.java
@@ -1,11 +1,13 @@
 package com.ifpb.caelestiabackend.controller.achievement;
 
 import com.ifpb.caelestiabackend.domain.entities.Achievement.Achievement;
+import com.ifpb.caelestiabackend.domain.entities.Achievement.BonusType;
 import com.ifpb.caelestiabackend.domain.entities.Module;
 import com.ifpb.caelestiabackend.repository.AchievementRepository;
 import com.ifpb.caelestiabackend.services.achievement.AchievementService;
 import com.ifpb.caelestiabackend.util.AchievementFactory;
 import com.ifpb.caelestiabackend.util.ModuleFactory;
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -34,9 +36,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(IAchievementController.class)
 @DisplayName("AchievementController tests")
+@Log4j2
 class AchievementControllerTest {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(AchievementControllerTest.class);
 
     @Autowired
     private MockMvc mockMvc;
@@ -225,6 +226,46 @@ class AchievementControllerTest {
                         "\"urlImage\":\"https://urlqualquer.com.br\"," +
                         "\"module\":{\"id\":1,\"name\":\"Astronomia antiga\"}}",
                 result.getResponse().getContentAsString()
+        );
+    }
+
+    @Test
+    public void shouldUpdateAndReturnAAchievement() throws Exception {
+        Achievement acToUpdate = new Achievement();
+        acToUpdate.setAchievementName("Novo nome da conquista");
+        acToUpdate.setBonusType(BonusType.PRATICAL_LESSON);
+
+        Module expectedModuleOfAc = ModuleFactory.makePersistedModule();
+        Achievement expectedAc = AchievementFactory.makePersistedAchievement();
+        expectedAc.setAchievementName("Novo nome da conquista");
+        expectedAc.setBonusType(BonusType.PRATICAL_LESSON);
+        expectedAc.setModule(expectedModuleOfAc);
+
+        Mockito.when(achievementService.update(ArgumentMatchers.anyLong(), ArgumentMatchers.eq(acToUpdate)))
+                .thenReturn(expectedAc);
+
+        log.info("Before Request");
+        log.info(acToUpdate);
+        RequestBuilder request = MockMvcRequestBuilders
+                .put("/achievement/{id}", 1L)
+                .accept(MediaType.APPLICATION_JSON)
+                .content("{" +
+                        "\"achievementName\":\"Novo nome da conquista\"," +
+                        "\"bonusType\":\"PRATICAL_LESSON\"" +
+                        "}")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Assertions.assertEquals("{\"id\":1,\"achievementName\":\"Novo nome da conquista\"," +
+                        "\"description\":\"+5% de xp em lições do tipo teórica\"," +
+                        "\"baseBonusPercentage\":0.5," +
+                        "\"bonusType\":\"PRATICAL_LESSON\"," +
+                        "\"urlImage\":\"https://urlqualquer.com.br\"," +
+                        "\"module\":{\"id\":1,\"name\":\"Astronomia antiga\"}}"
+                ,result.getResponse().getContentAsString()
         );
     }
 }
