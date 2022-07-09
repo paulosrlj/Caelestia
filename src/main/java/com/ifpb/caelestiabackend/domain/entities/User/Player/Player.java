b4b0a00fe73fba1bea3598df4f78c53bc4a40c3b
paulo <paulo.leite@academico.ifpb.edu.br>
2022-07-09 19:53:34 -0300

    feat: add field validations

--- src/main/java/com/ifpb/caelestiabackend/domain/entities/User/Player/Player.java
+++ src/main/java/com/ifpb/caelestiabackend/domain/entities/User/Player/Player.java
@@ -1,5 +1,6 @@
 package com.ifpb.caelestiabackend.domain.entities.User.Player;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -8,6 +9,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
@@ -24,19 +28,26 @@ public class Player {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Email(regexp = ".+[@].+[\\.].+")
     private String email;
 
+    @NotBlank(message = "A senha não pode estar em branco")
+    @NotNull(message = "A senha não pode ser nula")
     private String passwordHash;
 
+    @NotBlank(message = "O nome não pode estar em branco")
+    @NotNull(message = "O nome não pode ser nulo")
     private String name;
 
+    @NotBlank(message = "O apelido não pode estar em branco")
+    @NotNull(message = "O apelido não pode ser nulo")
     private String nickname;
 
-    private Long totalPoints;
+    private Long totalPoints = 0L;
 
-    private Integer level;
+    private Integer level = 1;
 
-    private Integer coins;
+    private Integer coins = 0;
 
     @CreationTimestamp
     @EqualsAndHashCode.Exclude
