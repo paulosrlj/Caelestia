658fd3e3b6ee1679b7b35080a4c471959d0118d6
paulo <paulo.leite@academico.ifpb.edu.br>
2022-07-11 17:26:53 -0300

    test: mustDeleteAPlayer

--- src/test/java/com/ifpb/caelestiabackend/repository/PlayerRepositoryTest.java
+++ src/test/java/com/ifpb/caelestiabackend/repository/PlayerRepositoryTest.java
@@ -1,8 +1,11 @@
 package com.ifpb.caelestiabackend.repository;
 
 import com.ifpb.caelestiabackend.domain.entities.Module;
+import com.ifpb.caelestiabackend.domain.entities.PraticalLesson.PraticalLesson;
 import com.ifpb.caelestiabackend.domain.entities.User.Player.Player;
+import com.ifpb.caelestiabackend.util.factories.ModuleFactory;
 import com.ifpb.caelestiabackend.util.factories.PlayerFactory;
+import com.ifpb.caelestiabackend.util.factories.PraticalLessonFactory;
 import lombok.extern.log4j.Log4j2;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -61,5 +64,17 @@ class PlayerRepositoryTest {
         Optional<Player> playerFound = playerRepository.findById(expectedPlayer.getId());
 
         Assertions.assertThat(expectedPlayer).isEqualTo(playerFound.get());
+    }
+
+    @Test
+    public void mustDeleteAPlayer() {
+        Player player = PlayerFactory.makePlayer();
+        Player persistedPlayer = playerRepository.save(player);
+
+        playerRepository.deleteById(persistedPlayer.getId());
+
+        Optional<Player> playerFound = playerRepository.findById(persistedPlayer.getId());
+
+        Assertions.assertThat(playerFound.isPresent()).isFalse();
     }
 }
